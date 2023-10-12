812d6c10738627185cc5cbd1e3e67be786f7e439
STUDENT Keith M. Varney <Keith.M.Varney@students.ittralee.ie>
2023-10-12 10:56:19 +0100

    tied rat's movement direction to the camera rotation, allowed camera rotation Signed-off-by: STUDENT Keith M. Varney <Keith.M.Varney@students.ittralee.ie>

--- MyRadProject/Assets/ratControl.cs
+++ MyRadProject/Assets/ratControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ratControl : MonoBehaviour
 {
-    float currentSpeed = 6;
+    public float currentSpeed = 6;
     private float turningSpeed = 700;
     Rigidbody rb;
     Vector3 explosive = new Vector3(0, -5f, 0);
@@ -12,7 +12,7 @@ public class ratControl : MonoBehaviour
     float ratHealth;
     public Health healthBar;
     public playerTracker cam;
-    Vector3 oldPosition;
+    float camRot;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,72 +24,71 @@ public class ratControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        oldPosition = transform.forward;
+        camRot = cam.transform.rotation.eulerAngles.y;
         rb.freezeRotation = true;
         ratHealth -= (12 * Time.deltaTime);
         healthBar.setHealth(ratHealth);
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
-            if (transform.rotation.eulerAngles.y >= 235 && transform.rotation.eulerAngles.y < 43)
+            if (transform.rotation.eulerAngles.y >= ((235 + camRot + 360) % 360) && transform.rotation.eulerAngles.y < ((43 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 235 && transform.rotation.eulerAngles.y > 47)
+            else if (transform.rotation.eulerAngles.y < ((235 + camRot + 360) % 360) && transform.rotation.eulerAngles.y > ((47 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 45 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, ((45 + camRot + 360) % 360) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
-                cam.diagionalMove(transform.forward, Vector3.right, currentSpeed);
             }
         }
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
-            if (transform.rotation.eulerAngles.y > 317 && transform.rotation.eulerAngles.y <= 135)
+            if (transform.rotation.eulerAngles.y > ((317 + camRot + 360) % 360) && transform.rotation.eulerAngles.y <= ((135+ camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 313 && transform.rotation.eulerAngles.y > 135)
+            else if (transform.rotation.eulerAngles.y < ((313 + camRot + 360) % 360) && transform.rotation.eulerAngles.y > ((135 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 315 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, ((315 + camRot + 360) % 360) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
             }
         }
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
         {
-            if (transform.rotation.eulerAngles.y >= 315 && transform.rotation.eulerAngles.y < 133)
+            if (transform.rotation.eulerAngles.y >= ((315 + camRot + 360) % 360) && transform.rotation.eulerAngles.y < ((133 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 315 && transform.rotation.eulerAngles.y > 137)
+            else if (transform.rotation.eulerAngles.y < ((315 + camRot + 360) % 360) && transform.rotation.eulerAngles.y > ((137 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 135 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, ((135 + camRot + 360) % 360) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
             }
         }
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
         {
-            if (transform.rotation.eulerAngles.y >= 45 && transform.rotation.eulerAngles.y < 223)
+            if (transform.rotation.eulerAngles.y >= ((45 + camRot + 360) % 360) && transform.rotation.eulerAngles.y < ((223 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 45 && transform.rotation.eulerAngles.y > 227)
+            else if (transform.rotation.eulerAngles.y < ((45 + camRot + 360) % 360) && transform.rotation.eulerAngles.y > ((227 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 225 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, ((225 + camRot + 360) % 360) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
             }
         }
@@ -97,76 +96,73 @@ public class ratControl : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W) &! Input.GetKey(KeyCode.D) &! Input.GetKey(KeyCode.A))
         {
-            if (transform.rotation.eulerAngles.y > 180 && transform.rotation.eulerAngles.y < 358)
+            if (transform.rotation.eulerAngles.y > ((180 + camRot + 360) % 360) && transform.rotation.eulerAngles.y < ((358 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y <= 180 && transform.rotation.eulerAngles.y > 2)
+            else if (transform.rotation.eulerAngles.y <= ((180 + camRot + 360) % 360) && transform.rotation.eulerAngles.y > ((2 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 2)
+            else if (transform.rotation.eulerAngles.y < ((2 + camRot + 360) % 360))
             {
-                transform.Rotate(Vector3.down, transform.rotation.eulerAngles.y);
-                transform.position += currentSpeed * transform.forward * Time.deltaTime;
-                cam.forwardMove(transform.forward, currentSpeed);
+                transform.Rotate(Vector3.down, transform.rotation.eulerAngles.y + 360);
+                //transform.Rotate(Vector3.up, (transform.rotation.eulerAngles.y + camRot + 360) % 360);
+                transform.position += currentSpeed * cam.transform.forward * Time.deltaTime;
             }
             else
             {
-                transform.Rotate(Vector3.up, 360 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, (360 + camRot) % 360 - transform.rotation.eulerAngles.y);
             }
         }
 
         if (Input.GetKey(KeyCode.S) &! Input.GetKey(KeyCode.D) &! Input.GetKey(KeyCode.A))
         {
-            if (transform.rotation.eulerAngles.y < 178)
+            if (transform.rotation.eulerAngles.y < ((178 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y > 182)
+            else if (transform.rotation.eulerAngles.y > ((182 + camRot + 360) % 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 180 - transform.rotation.eulerAngles.y);
-                transform.position += currentSpeed * transform.forward * Time.deltaTime;
-                cam.forwardMove(transform.forward, currentSpeed);
+                transform.Rotate(Vector3.up, ((180 + camRot+ 360) % 360) - transform.rotation.eulerAngles.y);
+                transform.position -= currentSpeed * cam.transform.forward * Time.deltaTime;
             }
         }
 
         if (Input.GetKey(KeyCode.A) &! Input.GetKey(KeyCode.W) &! Input.GetKey(KeyCode.S))
         {
-            if (transform.rotation.eulerAngles.y > 272 && transform.rotation.eulerAngles.y <= 90)
+            if (transform.rotation.eulerAngles.y > (272 + camRot) && transform.rotation.eulerAngles.y <= (90 + camRot))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 268 && transform.rotation.eulerAngles.y > 90)
+            else if (transform.rotation.eulerAngles.y < (268 + camRot) && transform.rotation.eulerAngles.y > (90 + camRot))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
             else 
             {
-                transform.Rotate(Vector3.up, 270 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, (270 + camRot) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
-                cam.sideMove(Vector3.left, currentSpeed);
             }
         }
         if (Input.GetKey(KeyCode.D) &! Input.GetKey(KeyCode.W) &! Input.GetKey(KeyCode.S))
         {
-            if (transform.rotation.eulerAngles.y >= 270 && transform.rotation.eulerAngles.y < 88)
+            if (transform.rotation.eulerAngles.y >= (270 + camRot) && transform.rotation.eulerAngles.y < (88 + camRot))
             {
                 transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
             }
-            else if (transform.rotation.eulerAngles.y < 270 && transform.rotation.eulerAngles.y > 92)
+            else if (transform.rotation.eulerAngles.y < (270 + camRot) && transform.rotation.eulerAngles.y > (92 + camRot + 360))
             {
                 transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up, 90 - transform.rotation.eulerAngles.y);
+                transform.Rotate(Vector3.up, (90 + camRot) - transform.rotation.eulerAngles.y);
                 transform.position += currentSpeed * transform.forward * Time.deltaTime;
-                cam.sideMove(Vector3.right, currentSpeed);
             }
         }
         if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0f)
